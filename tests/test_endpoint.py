c414f17693dcb6fbb4872ba5809c6bf54046b14f
matq <mathieu.quilliec@gmail.com>
2022-11-18 11:12:53 +0100

    push project

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -10,19 +10,21 @@ from fixtures import (
 
 client = TestClient(app)
 
+credentials = ("mathieu", "quilliec")
+
 
 def test_ReadStarneighboursEndpoint_ReturnsStatusCode200():
-    response = client.get(endpoint_url, auth=("mathieu", "quilliec"))
+    response = client.get(endpoint_url, auth=credentials)
     assert response.status_code == 200
 
 
 def test_ReadStarneighboursEndpoint_ReturnsListOfRepos(request_mock_all_api_calls):
-    response = client.get(endpoint_url, auth=("mathieu", "quilliec"))
+    response = client.get(endpoint_url, auth=credentials)
     assert isinstance(response.json(), list) is True
 
 
 def test_ReadStarneighboursEndpoint_ReturnsStatusCode403(
     request_mock_user_starred_api_rate_limit,
 ):
-    response = client.get(endpoint_url, auth=("mathieu", "quilliec"))
+    response = client.get(endpoint_url, auth=credentials)
     assert response.status_code == 403
